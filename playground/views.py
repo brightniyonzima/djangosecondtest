558a933afa7e782e9148c33e1bf1192ed877800b
brightniyonzima <brightniyonzima@gmail.com>
2023-02-17 16:16:01 +0300

    add plaground app with corresponding functionality

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+    print(thislist[2:5])
+
+    if "apple" in thislist:
+        print("Yes, 'apple' is in the fruits list")
+
+    for x in thislist:
+        print(x)
+
+    return HttpResponse('Hello Django!')
 
 # Create your views here.
